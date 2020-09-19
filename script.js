558cea3847cd6e61972ef9a763f04706a55f3ac7
cgriffin332 <cgriffin332@gmail.com>
2020-09-18 22:14:21 -0400

    added the links to projects.

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(".submitBtn").on("click", function(event){
+        event.preventDefault();
+        $("#name").val("");
+        $("#email").val("");
+        $("#message").val("");
+    })
+})
