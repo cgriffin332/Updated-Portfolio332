54948b9d63cfb5804f8fdca708879344fa699bff
cgriffin332 <cgriffin332@gmail.com>
2020-09-18 23:59:21 -0400

    ran prettier

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-$(document).ready(function(){
-    $(".submitBtn").on("click", function(event){
-        event.preventDefault();
-        $("#name").val("");
-        $("#email").val("");
-        $("#message").val("");
-    })
-})
+$(document).ready(function () {
+  $(".submitBtn").on("click", function (event) {
+    event.preventDefault();
+    $("#name").val("");
+    $("#email").val("");
+    $("#message").val("");
+  });
+});
